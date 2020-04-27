80de853e14d4b63da0aa9570cd5adc4025b7d2bc
Gnottero <gnotterodev@gmail.com>
2020-04-27 14:46:50 +0200

    Now the script will automatically generate the function tags, the main and the setup function as well.

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Nucleus
 A simple datapack template generator written in Python3
 
-This Python Script require the 'Requests' module to work
+This Python Script require the 'Requests' module to work.
+You can install it using the following command:
+`pip install requests`
 
 You simply need to provide a couple of information and it will automatically generate a datapack template that follows the official conventions
 If you want more informations about the conventions check out:
   
   - The official discord server: https://discord.gg/whFfamE
   - The official conventions website: https://ooboomberoo.github.io/mcdatapacks-wiki/index.html
+
+If you want to take a look at my datapacks, check out both my PlanetMinecraft and Github pages:
+
+  - PlanetMinecraft page: https://www.planetminecraft.com/member/gnottero/
+  - Github page: https://github.com/Gnottero
--- nucleus.py
+++ nucleus.py
@@ -22,23 +22,27 @@ skull_value = skull_value_rq.json()['properties'][0]['value']
 
 #> Starting the generation phase <#
 
+#> Generating all the needed folder <#
 g_adv_path = f"./{dp_name}/data/global/advancements"
 dp_adv_path = f"./{dp_name}/data/{dev_name.lower()}/advancements/{namespace}"
+mc_tags_path = f"./{dp_name}/data/minecraft/tags/functions"
+dp_tags_path = f"./{dp_name}/data/{dev_name.lower()}/tags/functions/{namespace}"
+dp_fun_path = f"./{dp_name}/data/{dev_name.lower()}/functions/{namespace}"
+
 
 try:
     os.makedirs(g_adv_path)
-except FileExistsError:
-    pass
-
-try:
     os.makedirs(dp_adv_path)
+    os.makedirs(mc_tags_path)
+    os.makedirs(dp_tags_path)
+    os.makedirs(dp_fun_path)
 except FileExistsError:
     pass
 
 
 with open(f'./{dp_name}/pack.mcmeta', 'w') as f:
-    f.write(f'''
-{{
+    f.write(
+f'''{{
     "pack": {{
         "pack_format": 5,
         "description": "{dp_name} by {dev_name}"
@@ -48,8 +52,8 @@ with open(f'./{dp_name}/pack.mcmeta', 'w') as f:
     )
     
 with open(f'{g_adv_path}/root.json', 'w') as f:
-    f.write(f'''
-{{
+    f.write(
+f'''{{
     "display": {{
         "title": "Installed Datapacks",
         "description": "",
@@ -70,8 +74,8 @@ with open(f'{g_adv_path}/root.json', 'w') as f:
     )
 
 with open(f'{g_adv_path}/{namespace}.json', 'w') as f:
-    f.write(f'''
-{{
+    f.write(
+f'''{{
     "display": {{
         "title": "{dev_name}",
         "description": "",
@@ -93,8 +97,8 @@ with open(f'{g_adv_path}/{namespace}.json', 'w') as f:
     )
 
 with open(f'{dp_adv_path}/{namespace}.json', 'w') as f:
-    f.write(f'''
-{{
+    f.write(
+f'''{{
     "display": {{
         "title": "{dp_name.title()}",
         "description": "{dp_desc}",
@@ -114,14 +118,70 @@ with open(f'{dp_adv_path}/{namespace}.json', 'w') as f:
     '''
     )
 
+with open(f'{mc_tags_path}/load.json', 'w') as f:
+    f.write(
+f'''{{
+    "values": [
+        "#{dev_name.lower()}:{namespace}/load"
+    ]
+}}
+    '''
+    )
 
-print(f''' 
-Template generated successfully with the following info:
+with open(f'{mc_tags_path}/tick.json', 'w') as f:
+    f.write(
+f'''{{
+    "values": [
+        "#{dev_name.lower()}:{namespace}/loop"
+    ]
+}}
+    '''
+    )
+
+with open(f'{dp_tags_path}/load.json', 'w') as f:
+    f.write(
+f'''{{
+    "values": [
+        "{dev_name.lower()}:{namespace}/setup"
+    ]
+}}
+    '''
+    )
+
+with open(f'{dp_tags_path}/loop.json', 'w') as f:
+    f.write(
+f'''{{
+    "values": [
+        "{dev_name.lower()}:{namespace}/main"
+    ]
+}}
+    '''
+    )
+
+with open(f'{dp_fun_path}/setup.mcfunction', 'w') as f:
+    f.write(
+    f'''# This function will run on datapack loading
+
+
+    '''
+    )
+
+with open(f'{dp_fun_path}/main.mcfunction', 'w') as f:
+    f.write(
+    f'''# This is the main function, that will run once per tick
+
+
+    '''
+    )
+
+print(
+    f''' Template generated successfully with the following info:
     
     Datapack developer: {dev_name}
     Datapack name: {dp_name}
     Datapack namespace: {namespace}
     Datapack item: minecraft:{dp_item}
     Datapack description: {dp_desc}
+
 '''
 )
