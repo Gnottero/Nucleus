63f62bf5a22e2dfc5adc6b7dd80954b90303a4ed
Gnottero <gnotterodev@gmail.com>
2020-04-28 22:30:08 +0200

    Added the possibility to choose the function names and made the code more readable

--- nucleus.py
+++ nucleus.py
@@ -5,14 +5,38 @@
 #> Importing all the required libraries <#
 import os
 import requests
+import json
 
 #> Setupping the variables <#
 dev_name = input('Please insert your name (Minecraft Username): ')
 dp_name = input('Please insert your datapack name: ')
 dp_item = input('Please insert the id of the item that will be displayed in the advancement: ')
 dp_desc = input('Please insert the description of the datapack: ')
+main_name = input('Please insert the name of the main function: ')
+load_name = input('Please insert the name of the load function: ')
 namespace = dp_name.replace(" ", "_").lower()
 
+
+#> Adding default values if the description or the item or the main fun name or the load fun name are not defined <#
+if len(dp_item) == 0:
+    dp_item= 'name_tag'
+else:
+    pass
+
+if len(dp_desc) == 0:
+    dp_desc = 'This is the pack description'
+else:
+    pass
+
+if len(main_name) == 0:
+    main_name = 'main'
+else:
+    pass
+
+if len(load_name) == 0:
+    load_name = 'setup'
+else:
+    pass
 
 #> Requesting player data from the Mojang API <#
 uuid_rq = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{dev_name}')
@@ -40,142 +64,67 @@ except FileExistsError:
     pass
 
 
-with open(f'./{dp_name}/pack.mcmeta', 'w') as f:
-    f.write(
-f'''{{
-    "pack": {{
-        "pack_format": 5,
-        "description": "{dp_name} by {dev_name}"
-    }}
-}}
-    '''
-    )
+def gen_pack_mcmeta(dp_name,dev_name):
+    pack = {"pack": {"pack_format": 5,"description": f"{dp_name} by {dev_name}"}}
+    with open(f'./{dp_name}/pack.mcmeta', 'w') as f:
+        f.write(json.dumps(pack, indent=5, sort_keys=True))
+
+
+def global_advancements(g_adv_path,namespace,dev_name,skull_value):
+    root = {"display": {"title": "Installed Datapacks","description": "","icon": {"item": "minecraft:knowledge_book"},"background": "minecraft:textures/block/gray_concrete.png","show_toast": False,"announce_to_chat": False},"criteria": {"trigger": {"trigger": "minecraft:tick"}}}
+    dev = {"display": {"title": f"{dev_name}","description": "","icon": {"item": "minecraft:player_head","nbt": f"{{SkullOwner:{{Name: \"{dev_name}\", Properties: {{textures: [{{Value: \"{skull_value}\"}}]}}}}}}"},"show_toast": False,"announce_to_chat": False},"parent": "global:root","criteria": {"trigger": {"trigger": "minecraft:tick"}}}
     
-with open(f'{g_adv_path}/root.json', 'w') as f:
-    f.write(
-f'''{{
-    "display": {{
-        "title": "Installed Datapacks",
-        "description": "",
-        "icon": {{
-            "item": "minecraft:knowledge_book"
-        }},
-        "background": "minecraft:textures/block/gray_concrete.png",
-        "show_toast": false,
-        "announce_to_chat": false
-    }},
-    "criteria": {{
-        "trigger": {{
-            "trigger": "minecraft:tick"
-        }}
-    }}
-}}
-    '''
-    )
-
-with open(f'{g_adv_path}/{namespace}.json', 'w') as f:
-    f.write(
-f'''{{
-    "display": {{
-        "title": "{dev_name}",
-        "description": "",
-        "icon": {{
-            "item": "minecraft:player_head",
-            "nbt": "{{SkullOwner:{{Name:\\"{dev_name}\\",Properties:{{textures:[{{Value:\\"{skull_value}\\"}}]}}}}}}"
-        }},
-        "show_toast": false,
-        "announce_to_chat": false
-    }},
-    "parent": "global:root",
-    "criteria": {{
-        "trigger": {{
-            "trigger": "minecraft:tick"
-        }}
-    }}
-}}
-    '''
-    )
-
-with open(f'{dp_adv_path}/{namespace}.json', 'w') as f:
-    f.write(
-f'''{{
-    "display": {{
-        "title": "{dp_name.title()}",
-        "description": "{dp_desc}",
-        "icon": {{
-            "item": "minecraft:{dp_item}"
-        }},
-        "announce_to_chat": false,
-        "show_toast": false
-    }},
-    "parent": "global:{namespace}",
-    "criteria": {{
-        "trigger": {{
-            "trigger": "minecraft:tick"
-        }}
-    }}
-}}
-    '''
-    )
-
-with open(f'{mc_tags_path}/load.json', 'w') as f:
-    f.write(
-f'''{{
-    "values": [
-        "#{dev_name.lower()}:{namespace}/load"
-    ]
-}}
-    '''
-    )
-
-with open(f'{mc_tags_path}/tick.json', 'w') as f:
-    f.write(
-f'''{{
-    "values": [
-        "#{dev_name.lower()}:{namespace}/loop"
-    ]
-}}
-    '''
-    )
-
-with open(f'{dp_tags_path}/load.json', 'w') as f:
-    f.write(
-f'''{{
-    "values": [
-        "{dev_name.lower()}:{namespace}/setup"
-    ]
-}}
-    '''
-    )
-
-with open(f'{dp_tags_path}/loop.json', 'w') as f:
-    f.write(
-f'''{{
-    "values": [
-        "{dev_name.lower()}:{namespace}/main"
-    ]
-}}
-    '''
-    )
-
-with open(f'{dp_fun_path}/setup.mcfunction', 'w') as f:
-    f.write(
-    f'''# This function will run on datapack loading
+    with open(f'{g_adv_path}/root.json', 'w') as f:
+        f.write(json.dumps(root, indent=5, sort_keys=True))
+    with open(f'{g_adv_path}/{namespace}.json', 'w') as f:
+        f.write(json.dumps(dev, indent=5, sort_keys=True))
 
 
-    '''
-    )
-
-with open(f'{dp_fun_path}/main.mcfunction', 'w') as f:
-    f.write(
-    f'''# This is the main function, that will run once per tick
+def dp_advancement(dp_adv_path,namespace,dp_name,dp_desc,dp_item):
+    dp_adv = {"display": {"title": f"{dp_name.title()}","description": f"{dp_desc}","icon": {"item": f"minecraft:{dp_item}"},"announce_to_chat": False,"show_toast": False},"parent": f"global:{namespace}","criteria": {"trigger": {"trigger": "minecraft:tick"}}}
+    with open(f'{dp_adv_path}/{namespace}.json', 'w') as f:
+        f.write(json.dumps(dp_adv, indent=5, sort_keys=True))
 
 
-    '''
-    )
+def mc_tags(mc_tags_path,dev_name,namespace):
+    mc_load = {"values": [f"#{dev_name.lower()}:{namespace}/load"]}
+    mc_tick = {"values": [f"#{dev_name.lower()}:{namespace}/loop"]}
+    
+    with open(f'{mc_tags_path}/load.json', 'w') as f:
+        f.write(json.dumps(mc_load, indent=5, sort_keys=True))
+    with open(f'{mc_tags_path}/tick.json', 'w') as f:
+        f.write(json.dumps(mc_tick, indent=5, sort_keys=True))
+
+
+def dp_tags(dp_tags_path,dev_name,namespace,main_name,load_name):
+    ns_load = {"values": [f"{dev_name.lower()}:{namespace}/{load_name.lower()}"]}
+    ns_loop = {"values": [f"{dev_name.lower()}:{namespace}/{main_name.lower()}"]}
+    
+    with open(f'{dp_tags_path}/load.json', 'w') as f:
+        f.write(json.dumps(ns_load, indent=5, sort_keys=True))
+    with open(f'{dp_tags_path}/loop.json', 'w') as f:
+        f.write(json.dumps(ns_loop, indent=5, sort_keys=True))
+
+
+def dp_fun(dp_fun_path,main_name,load_name):
+    with open(f'{dp_fun_path}/{main_name.lower()}.mcfunction', 'w') as f:
+        f.write('#> This is the main function, that will run once per tick')
+    with open(f'{dp_fun_path}/{load_name.lower()}.mcfunction', 'w') as f:
+        f.write('#> This function will run on datapack loading')
+
+
+
+#> Calling all the functions <#
+gen_pack_mcmeta(dp_name,dev_name)
+global_advancements(g_adv_path,namespace,dev_name,skull_value)
+dp_advancement(dp_adv_path,namespace,dp_name,dp_desc,dp_item)
+mc_tags(mc_tags_path,dev_name,namespace)
+dp_tags(dp_tags_path,dev_name,namespace,main_name,load_name)
+dp_fun(dp_fun_path,main_name,load_name)
+
 
 print(
-    f''' Template generated successfully with the following info:
+    f'''Template generated successfully with the following info:
     
     Datapack developer: {dev_name}
     Datapack name: {dp_name}
